a56fe0ac2a0b77608639283d2f00a239fa0579ea
Joe Zhou <zhoushuqun@gmail.com>
2025-03-22 13:24:38 +0800

    Add cache check method.

--- vividseats-client-cached/vividseats-client-cached-core/src/main/java/org/oxerr/vividseats/client/cached/inventory/VividSeatsCachedListingService.java
+++ vividseats-client-cached/vividseats-client-cached-core/src/main/java/org/oxerr/vividseats/client/cached/inventory/VividSeatsCachedListingService.java
@@ -1,8 +1,23 @@
 package org.oxerr.vividseats.client.cached.inventory;
 
+import java.io.IOException;
+
 import org.oxerr.ticket.inventory.support.cached.CachedListingService;
 import org.oxerr.vividseats.client.model.inventory.BrokerListing;
 
 public interface VividSeatsCachedListingService
 	extends CachedListingService<String, String, BrokerListing, VividSeatsListing, VividSeatsEvent> {
+
+	/**
+	 * Check all listings and delete which not in cache.
+	 */
+	void check() throws IOException;
+
+	/**
+	 * Check all listings and delete which not in cache.
+	 *
+	 * @param options the check options.
+	 */
+	void check(CheckOptions options) throws IOException;
+
 }
--- vividseats-client-cached/vividseats-client-cached-redisson/src/main/java/org/oxerr/vividseats/client/cached/redisson/inventory/RedissonCachedListingService.java
+++ vividseats-client-cached/vividseats-client-cached-redisson/src/main/java/org/oxerr/vividseats/client/cached/redisson/inventory/RedissonCachedListingService.java
@@ -1,19 +1,29 @@
 package org.oxerr.vividseats.client.cached.redisson.inventory;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
+import java.util.stream.Collectors;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.oxerr.ticket.inventory.support.cached.redisson.ListingConfiguration;
 import org.oxerr.ticket.inventory.support.cached.redisson.RedissonCachedListingServiceSupport;
 import org.oxerr.ticket.inventory.support.cached.redisson.Status;
+import org.oxerr.vividseats.client.cached.inventory.CheckOptions;
 import org.oxerr.vividseats.client.cached.inventory.VividSeatsCachedListingService;
 import org.oxerr.vividseats.client.cached.inventory.VividSeatsEvent;
 import org.oxerr.vividseats.client.cached.inventory.VividSeatsListing;
+import org.oxerr.vividseats.client.inventory.BrokerListingQuery;
 import org.oxerr.vividseats.client.inventory.ListingService;
 import org.oxerr.vividseats.client.model.inventory.BrokerListing;
 import org.redisson.api.RedissonClient;
@@ -21,6 +31,8 @@ import org.redisson.api.RedissonClient;
 public class RedissonCachedListingService
 	extends RedissonCachedListingServiceSupport<String, String, BrokerListing, VividSeatsListing, VividSeatsEvent, VividSeatsCachedListing>
 	implements VividSeatsCachedListingService {
+
+	private final Logger log = LogManager.getLogger();
 
 	private final ListingService listingService;
 
@@ -130,6 +142,93 @@ public class RedissonCachedListingService
 	@Override
 	protected VividSeatsCachedListing toCached(VividSeatsEvent event, VividSeatsListing listing, Status status) {
 		return new VividSeatsCachedListing(new VividSeatsCachedEvent(event), listing, status);
+	}
+
+	@Override
+	public void check() throws IOException {
+		check(CheckOptions.defaults());
+	}
+
+	@Override
+	public void check(CheckOptions options) throws IOException {
+		List<CompletableFuture<Void>> tasks = Collections.synchronizedList(new ArrayList<CompletableFuture<Void>>());
+
+		// Ticket ID to cache name.
+		Map<String, String> ticketIdToCacheName = this.getCacheNamesStream(options.chunkSize())
+			.flatMap(cacheName ->
+				this.getCache(cacheName).keySet().stream()
+					.map(id -> Map.entry(id, cacheName))
+			)
+			.collect(Collectors.toUnmodifiableMap(Map.Entry::getKey, Map.Entry::getValue));
+
+		// All listings on the marketplace.
+		List<BrokerListing> listings = this.listingService.get(new BrokerListingQuery());
+
+		// Delete listings which not in cache.
+		var deleteTasks = listings.stream()
+			.filter(listing -> !ticketIdToCacheName.containsKey(listing.getTicketId()))
+			.map(listing -> this.<Void>callAsync(() -> {
+				this.listingService.deleteListing(listing.getTicketId());
+				return null;
+			})).collect(Collectors.toUnmodifiableList());
+
+		tasks.addAll(deleteTasks);
+
+		// Create or update listings in cache.
+		listings.stream()
+			.filter(listing -> ticketIdToCacheName.containsKey(listing.getTicketId()))
+			.forEach(listing -> {
+				String cacheName = ticketIdToCacheName.get(listing.getTicketId());
+				VividSeatsCachedListing cachedListing = this.getCache(cacheName).get(listing.getTicketId());
+				if (cachedListing == null) {
+					// Double check the listing if it is not in cache.
+					// If the listing is not in cache, delete the listing from marketplace.
+					var task = this.<Void>callAsync(() -> {
+						this.listingService.deleteListing(listing.getTicketId());
+						return null;
+					});
+					tasks.add(task);
+				} else if (!isSame(listing, cachedListing.getRequest())) {
+					var task = this.<Void>callAsync(() -> {
+						var e = cachedListing.getEvent().toVividSeatsEvent();
+						var l = cachedListing.toVividSeatsListing();
+						var p = getPriority(e, l, cachedListing);
+
+						if (e.getVividSeatsEventId().equals(listing.getProductionId())) {
+							this.updateListing(e, l, cachedListing, p);
+						} else {
+							this.deleteListing(e, l.getId());
+						}
+						return null;
+					});
+					tasks.add(task);
+				}
+			});
+
+		// Create listings which in cache but not on the marketplace.
+		var createTasks = ticketIdToCacheName.entrySet().stream()
+			.filter(entry -> listings.stream().noneMatch(listing -> listing.getTicketId().equals(entry.getKey())))
+			.map(entry -> this.<Void>callAsync(() -> {
+				var cacheName = entry.getValue();
+				var cachedListing = this.getCache(cacheName).get(entry.getKey());
+				var e = cachedListing.getEvent().toVividSeatsEvent();
+				var l = cachedListing.toVividSeatsListing();
+				this.createListing(e, l);
+				return null;
+			})).collect(Collectors.toUnmodifiableList());
+
+		tasks.addAll(createTasks);
+
+		// Wait all tasks to complete.
+		log.debug("[check] checking size: {}", tasks::size);
+		CompletableFuture.allOf(tasks.toArray(CompletableFuture[]::new)).join();
+	}
+
+	private boolean isSame(BrokerListing listing, BrokerListing request) {
+		var equals = Objects.equals(listing, request);
+		log.debug("[isSame] listing: {}, request: {}, equals: {}", listing, request, equals);
+		// TODO: Compare other fields.
+		return true;
 	}
 
 }
